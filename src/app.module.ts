18f716e9e0566c613b7aaea04828d34fc8ae5ece
Eduardo Peredo Rivero <jrperedo@gmail.com>
2026-01-19 21:13:00 -0500

    add prisma implementatins

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [PrismaService, AppService],
+  providers: [AppService],
 })
 export class AppModule {}
