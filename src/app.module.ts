ad74150729ae6c0fed4a7c3e6e850f3f259e41db
Eduardo Peredo Rivero <jrperedo@gmail.com>
2026-01-19 21:56:07 -0500

    eslint prettier fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
