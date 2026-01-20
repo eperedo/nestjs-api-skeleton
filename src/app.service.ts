1710c4d0bdfc1b1730030e886af5b0f77f080d5e
Eduardo Peredo Rivero <jrperedo@gmail.com>
2026-01-19 20:59:28 -0500

    add prisma integration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+  async getHello(): Promise<string> {
+    const user = await this.prisma.user.findFirst({
+      where: { id: '1' },
+    });
+
+    return user?.username ?? 'Hello World!';
   }
 }
---
+++ src/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from './generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    if (!process.env.DATABASE_URL)
+      throw new Error('DATABASE_URL is not defined');
+
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+    super({ adapter });
+  }
+}
